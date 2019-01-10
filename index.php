0b9a2581c92fafcaf92fa2d342afdc0d27d8aef6
Benjamin Brandtner <37274870+BenjaminBrandtner@users.noreply.github.com>
2019-01-10 16:39:07 +0100

    Simple Startseite angelegt

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Chatinga - Einloggen</title>
+</head>
+<body>
+	<h1>Herzlich Willkommen bei Chatinga!</h1>
+
+<?php
+if(isset($_SESSION["Nutzername"]))
+{
+	echo "Du bist angemeldet als " . $_SESSION["Nutzername"] . "<br>";
+	echo "<a href='logoutScript.php'>Ausloggen</a><br>";
+	echo "<a href='chatauswahl.php'>Zur Chatauswahl</a><br>";
+}
+else
+{
+	echo "<a href='loginPage.php'>Einloggen oder Registrieren</a><br>";
+}
+?>
+</body>
+</html>
