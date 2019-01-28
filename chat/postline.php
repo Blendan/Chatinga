fd0d76942780d378cca21334c8762f037d2067b0
lilsmoke871 <46438886+lilsmoke871@users.noreply.github.com>
2019-01-28 12:25:31 +0100

    Themeauswahl fertig

--- chat/postline.php
+++ chat/postline.php
@@ -1,7 +1,11 @@
+<?php session_start(); 
+include "../include.php/colorthemeFunctions.inc.php";
+$pdo = new PDO("mysql:host=localhost;dbname=Chatinga;charset=utf8", "root", "");
+?>
 <!DOCTYPE html>
 <html lang="de" dir="ltr">
   <head>
- <link rel="stylesheet" type="text/css" href="../css/postline.css">
+  	<link rel="stylesheet" type="text/css" href="../css/<?= retrieveThemeFilename($_SESSION["gewaehltesThema"], $pdo)?>/postline.css">
     <meta charset="utf-8">
     <title></title>
   </head>
